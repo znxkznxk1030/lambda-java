a61a8e3116d5a1c543516867b595da905434c02e
ysKim <znxkznxk1030@gmail.com>
2022-04-12 17:31:08 +0900

    java로 lambda개발하기

---
+++ HelloLambda/src/main/java/com/amazonaws/lambda/demo/HelloLambda.java
@@ -0,0 +1,15 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class HelloLambda implements RequestHandler<Object, String> {
+
+	@Override
+    public String handleRequest(Object input, Context context) {
+    	  context.getLogger().log("Input: " + input);
+    	  String output = "Hello, " + input + "!";
+    	  return output;
+    }
+
+}
